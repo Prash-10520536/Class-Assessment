e0f0220f098d1c6338b65b1aad804c21212803c6
Prashant Choudhari <60341906+Prash-10520536@users.noreply.github.com>
2020-03-24 09:32:48 +0000

    Update class-test.py
    symbols changes

--- class-test.py
+++ class-test.py
@@ -2,9 +2,9 @@ discount = float[0.5]
 
 class Shopping :
     
-    basket = {"eggs": 10, "rice" 15, "flour"; 20} #qantity
-    price = {"eggs": 8, "rice" 12, "flour"; 10} #price
+    basket = {"eggs": 10, "rice": 15, "flour": 20} #qantity
+    price = {"eggs": 8, "rice": 12, "flour": 10} #price
 
 def bill(food):
     total = 0
-for items in basket:
+    for items in basket:
