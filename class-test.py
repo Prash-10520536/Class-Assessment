30fe724f1af80ac4f00be0733bee0779db2b834e
Prashant Choudhari <60341906+Prash-10520536@users.noreply.github.com>
2020-03-24 09:30:05 +0000

    Update class-test.py

--- class-test.py
+++ class-test.py
@@ -1,1 +1,10 @@
+discount = float[0.5]
 
+class Shopping :
+    
+    basket = {"eggs": 10, "rice" 15, "flour"; 20} #qantity
+    price = {"eggs": 8, "rice" 12, "flour"; 10} #price
+
+def bill(food):
+    total = 0
+for items in basket:
