a4f80c9d658ad8084ad008995647329f7f46f455
Prashant Choudhari <60341906+Prash-10520536@users.noreply.github.com>
2020-03-24 10:00:28 +0000

    Update class-test.py

--- class-test.py
+++ class-test.py
@@ -1,4 +1,6 @@
-discount = float[0.5]
+discCode1 = float[0.5]
+discCode2 = float[0.8]
+discCode3 = float[0.010]
 
 class Shopping :
     
@@ -8,3 +10,21 @@ class Shopping :
 def bill(food):
     total = 0
     for items in basket:
+        if(price>0) :
+            if(price==8):
+                discount = price*discCode1
+            elif(price==12):
+                discount = price*discCode2
+            elif(price==10):
+                diccount = price*discCode3
+            
+            total = price - discount
+            return total
+                
+            
+        
+    
+    
+    
+
+    
