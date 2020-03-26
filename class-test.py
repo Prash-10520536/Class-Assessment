1f7cef8b8f600ff9bb173ad80961abd9ec0d34e9
Prashant Choudhari <60341906+Prash-10520536@users.noreply.github.com>
2020-03-26 00:07:43 +0000

    Update class-test.py

--- class-test.py
+++ class-test.py
@@ -1,38 +1,24 @@
-discount={'discCode1':0.5,'discCode2':0.8,'discCode3':0.10}
+discounts={'discCode1':0.5,'discCode2':0.8,'discCode3':0.10}
 
 class Shopping :
     
-    basket = {"eggs": 10, "rice": 15, "flour": 20} #qantity
-    price = {"eggs": 8, "rice": 12, "flour": 10} #price
+    price = {"eggs": 2, "rice": 3, "flour": 5} #price
+    
+    def __init__(self, price):
+        self.total = 0
+        self.price = price
+        self.items = {}
+    
 
-def bill(food):
-    total = 0
-    for items in basket:
+def doBuy():    
+    for price in basket.values():
         if(price>0) :
-            if(price==8):
-                discount = price*discCode1
-            elif(price==12):
-                discount = price*discCode2
-            elif(price==10):
-                diccount = price*discCode3
+            if(price==2):
+                discount = price*discounts["discCode1"]
+            elif(price==3):
+                discount = price*discounts["discCode2"]
+            elif(price==5):
+                diccount = price*discounts["discCode3"]
             
             total = price - discount
             return total
-
-
-
-def priceSort(bList):
-    lowP=len(bList)
-    for i in range(lowP-1):
-        for j in range(lP-1-i):
-            if bList[j] >bList[j+1]:
-                bList[j],bList[j+1]=bList[j+1],bList[j]
-    return bList
-
-print('total', 'priceList')
-        
-    
-    
-    
-
-    
