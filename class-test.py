20645bfb7ef9845e790e667112aded3f5087c4d5
Prashant Choudhari <60341906+Prash-10520536@users.noreply.github.com>
2020-03-26 17:56:18 +0000

    Update class-test.py

--- class-test.py
+++ class-test.py
@@ -19,3 +19,11 @@ class Shopping :
         for k,v in trolly.items():
             i+=prices[k]*trolly[k]*discounts[k]
             priceL.append(i)  
+            
+     def priceSort(self,bList):
+        lowP=len(bList)
+        for i in range(lowP-1):
+                for j in range(lowP-1-i):
+                    if bList[j] >bList[j+1]:
+                        bList[j],bList[j+1]=bList[j+1],bList[j]
+        return bList
