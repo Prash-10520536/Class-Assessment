eb0ab12e20aef94034bd89787600a69ded8f72a7
Prashant Choudhari <60341906+Prash-10520536@users.noreply.github.com>
2020-03-26 18:12:28 +0000

    Update class-test.py
    final commit

--- class-test.py
+++ class-test.py
@@ -1,6 +1,6 @@
-discounts={'rice':0.5,'eggs':0.8,'flour':0.10}
+discounts={'rice':0.5,'eggs':0.8,'flour':0.6} #discount
 prices = {"flour": 2, "eggs": 3, "rice": 5} #price
-trolly={}
+
 
 class Shopping :
     
@@ -8,23 +8,30 @@ class Shopping :
         self.__basket = basket
         
     #to add items and quants
+    trolly={}
     def addItems(self, items, quant):        
         trolly[items] = quant
         print(trolly)
         
-   #getting price with discount
+    #getting price with discount
     def getPrice(self):
         i=0
-        priceL=[]
+        priceL=[] #for price list
         for k,v in trolly.items():
-            i+=prices[k]*trolly[k]*discounts[k]
-            priceL.append(i)  
-            
-      def priceSort(self,bList):
+            i+=prices[k]*trolly[k]*discounts[k] #applying dicsount
+            priceL.append(i)        
+        
+        print(priceL) #to print after appending 
+        self.priceSort(priceL)
+        return i
+
+    #sorting using bubble sort
+    def priceSort(self,bList):
         lowP=len(bList)
         for i in range(lowP-1):
                 for j in range(lowP-1-i):
                     if bList[j] >bList[j+1]:
                         bList[j],bList[j+1]=bList[j+1],bList[j]
-                    print("Prices: High to Low ", bList[::-1])
+                    print("Price highest to lowest ", bList[::-1])
         return bList
+
